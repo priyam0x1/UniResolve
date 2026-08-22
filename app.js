f10825ccddc21108de9d872e04558174e62ea2a4
Priyam <priyampratim57@gmail.com>
2026-08-22 23:07:08 +0530

    Add initial file

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("You are at root");
+});
+
+app.listen(8080, () => {
+  console.log("Server is litsening at port 8080");
+});
