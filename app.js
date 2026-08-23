25fcb331aa9cfde2ef261f532452f221faa5cc54
Priyam <priyampratim57@gmail.com>
2026-08-23 23:56:19 +0530

    Sending mail through nodemailer

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("dotenv").config({ quiet: true });
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Complaint = require("./models/complaint.js");
 const Authority = require("./models/authority.js");
+const nodemailer = require("nodemailer");
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -22,7 +24,24 @@ async function main() {
 }
 
 // ====================================Nodemailer || Starting ==========================================
+const transporter = nodemailer.createTransport({
+  secure: true,
+  host: "smtp.gmail.com",
+  port: 465,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.APP_Password,
+  },
+});
 
+async function sendMail(to, sub, msg) {
+  await transporter.sendMail({
+    to: to,
+    subject: sub,
+    html: msg,
+  });
+  console.log("Email was sent");
+}
 // ====================================Nodemailer || Done ==========================================
 
 app.get("/home", (req, res) => {
@@ -33,8 +52,9 @@ app.get("/home/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/complaint", (req, res) => {
-  console.log(req.body.complaint);
+app.post("/complaint", async (req, res) => {
+  let complaint = new Complaint(req.body.complaint);
+  await complaint.save();
   res.redirect("/home");
 });
 
@@ -44,10 +64,52 @@ app.get("/admin", async (req, res) => {
 });
 
 app.post("/admin/:id", async (req, res) => {
-  console.log(req.params.id);
   let complaint = await Complaint.findById(req.params.id);
   complaint.forward_to = req.body.forward_to;
-  console.log(complaint);
+
+  let authority = await Authority.findOne({
+    authority: complaint.forward_to,
+  });
+  let to = authority.email;
+  let sub = complaint.title;
+  let msg = `
+    <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+
+        <h2>Complaint Forwarded - ${complaint.title}</h2>
+
+        <p>Dear Sir/Madam,</p>
+
+        <p>
+            A complaint has been forwarded to you for your attention.
+        </p>
+
+        <h3>Complaint Details</h3>
+
+        <p><strong>Student:</strong> ${complaint.fullname}</p>
+        <p><strong>University ID:</strong> ${complaint.uni_id}</p>
+        <p><strong>Department:</strong> ${complaint.dept}</p>
+        <p><strong>Category:</strong> ${complaint.category}</p>
+
+        <h3>Issue</h3>
+
+        <p>
+            ${complaint.description}
+        </p>
+
+        <p>
+            Kindly look into this matter and take the necessary action.
+        </p>
+
+        <p>
+            Regards,<br>
+            <strong>UniResolve Administration</strong>
+        </p>
+
+    </div>
+`;
+  // sendMail(to, sub, msg);
+  complaint.status = "forwarded";
+  await complaint.save();
   res.redirect("/admin");
 });
 
